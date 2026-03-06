e6aef1a9d591b79d0fc19dc0c2173448db164865
Son1017 <reig.vaeo.soiv099@gmail.com>
2026-03-06 11:21:51 -0500

    1.4

--- library.cpp
+++ library.cpp
@@ -1,4 +1,23 @@
 // library.cpp -- Implementation file for the Course and Student classes
 
 #include "library.h"
+#include <string>
+#include <iostream>
+
+StudentID::StudentID(std::string stname, std::string lstname, int _id): 
+first_name(stname), last_name(lstname), id(_id) {}
+
+void StudentID::display() const {
+    std::cout << "Student Last Name: " << last_name << std::endl;
+    std::cout << "Student First Name: " << first_name << std::endl;
+    std::cout << "Student ID: " << id << std::endl;
+}
+
+CourseID::CourseID(std::string _code, std::string _name): 
+code(_code), name(_name) {}
+
+void CourseID::display() const {
+    std::cout << "Course Name: " << name << std::endl;
+    std::cout << "Course Code: " << code << std::endl;   
+}
 
