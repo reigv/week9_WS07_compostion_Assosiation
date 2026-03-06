16a9f17383c285dada69c140993fcc876edfcc0c
caidur <reig.vaeo.soiv099@gmail.com>
2026-03-06 12:58:00 -0500

    1.9

--- library.h
+++ library.h
@@ -1,11 +1,25 @@
 // library.h -- Header file for the Course and Student classes
 // distinguishes between COMPOSTION & ASSOCIATION
 
+// Association -- general relationship 
+//              -- objs are independent (exist without the others)
+//              -- weakest
+//              -- poiters || reference
+
+// Composition  -- strong ownership
+                // -- the life of the part depends on the whole 
+                // -- destroyed && destroyed
+                // -- direct member obj
+
+
 #ifndef LIBRARY_H_
 #define LIBRARY_H_
 
 const int SIZE = 30;
 #include <string>
+
+//----------------------------------------------------------------
+
 class StudentID
 {
 private:
@@ -14,10 +28,37 @@ private:
     int id;
 
 public:
-    StudentID();
+    StudentID(std::string stname, std::string lstname, int _id);
     void display() const;
 
 };
 
+//----------------------------------------------------------------
+
+class CourseID {
+private:
+    std::string code;
+    std::string name;
+
+public:
+    CourseID(std::string _code, std::string _name);
+    void display() const;
+};
+
+//----------------------------------------------------------------
+
+class Course
+{
+private:
+    CourseID course_id_;
+    Student* students_
+
+};
+
+class Student 
+{
+private: 
+
+};
 
 #endif // LIBRARY_H_
---
+++ test_main.cpp
@@ -0,0 +1,11 @@
+// test purpose
+
+#include "library.h"
+
+int main() {
+    StudentID theguy("myname", "mylastname", 565);
+    theguy.display();
+
+    CourseID cs101("CS101", "Intro to Programming");
+    cs101.display();
+}
